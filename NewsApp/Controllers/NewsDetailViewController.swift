1272257ff55b3db04e7d85f84f80032ffcf91d89
dotm <desde.el.mar96@gmail.com>
2019-03-19 16:08:20 +0700

    add basic NewsDetail and connect it to NewsDetailViewController

--- NewsApp/Controllers/NewsDetailViewController.swift
+++ NewsApp/Controllers/NewsDetailViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class NewsDetailViewController: UIViewController {
 
+    //MARK: Lifecycle Hooks
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .blue
+        setupLayout()
     }
     
+    //MARK: Layout
+    final private func setupLayout(){
+        self.view.backgroundColor = .white
+        
+        setupNewsDetail()
+    }
+    final private func setupNewsDetail(){
+        let newsDetail = NewsDetail()
+        view.addSubview(newsDetail)
+        
+        newsDetail.translatesAutoresizingMaskIntoConstraints = false
+        newsDetail.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        newsDetail.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        newsDetail.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        newsDetail.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
 }
---
+++ NewsApp/Views/NewsDetail.swift
@@ -0,0 +1,31 @@
+//
+//  NewsDetail.swift
+//  NewsApp
+//
+//  Created by Yoshua Elmaryono on 19/03/19.
+//  Copyright © 2019 Yoshua Elmaryono. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetail: UIView {
+
+    //MARK: Initializers
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupLayout()
+    }
+    
+    //MARK: Layout
+    final private func setupLayout(){
+        self.backgroundColor = .gray
+    }
+
+}
